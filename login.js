f1d6aeb1f142e8490ba7119634b52dcf6e83240d
CatherineZeng <yczeng@mit.edu>
2017-07-10 14:04:42 -0500

    rm login

--- login.js
+++
@@ -1,7 +0,0 @@
-const r = new snoowrap({
-  userAgent: 'bitch 1.0 by /u/y0io',
-  clientId: 'eKI4ql4PrIWQOg',
-  clientSecret: 'SR_c42SFPeCzu9ItENzy9CMZzsA',
-  username: 'y0io',
-  password: 'Tolkien$420'
-});
